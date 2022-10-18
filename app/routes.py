5e0f772e40d349d6f40e39754d5d7009cec5b731
JoaoLucasVeras <joao.lucas.veras9@gmail.com>
2022-10-18 01:48:46 -0700

    Added template directory  - new file -> hello.html  - new file -> login.html Changes to Route.py  - imported render_template  - hello() now returns hello.html along with name variable  - login() now returns login.html

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
 from app import myapp_obj
+from flask import render_template
 
 @myapp_obj.route("/")
 
 def hello():
-    return "hello"
+    name = "ricardo"
+    return render_template('hello.html', names = name)
 
+@myapp_obj.route("/login")
 
+def login():
+    return render_template('login.html')
